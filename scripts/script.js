a4504e4364bf5e531dbb14456ba6c052d090d592
Bkeefe901 <bradyjokeefe@gmail.com>
2025-08-15 11:55:58 -0400

    Basic idea formed, filling in some basic detail and psuedo code outline

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// Create event listener to input for color
+    // If color doesnt match color in color dictionary send an alert
+
+// Create event listener to button
+    // When clicked it will create a li (and a checkbox-input and a label inside) and appends it to ul with the input submitted as the textContent of the li>label
+
+// Create event listern for the ul 
+    // when a checkbox is checked in a li it cyles the color from that label in the background of the body
