ffab1b30c248d3233de20e2fa343b27e65719dab
Bkeefe901 <bradyjokeefe@gmail.com>
2025-08-17 22:55:33 -0400

    Making progress on event listener for checkboxes

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 // Array of all named colors in css
 const Arr = [
-    "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkGrey", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", 
-    "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green", "GreenYellow", "Grey", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGreen", "LightGrey", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", 
+    "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkGrey", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue",
+    "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green", "GreenYellow", "Grey", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGreen", "LightGrey", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise",
     "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"
 ]
 let colorArr = [];
 
-for(color of Arr){
+for (color of Arr) {
     let lowColor = color.toLowerCase();
     colorArr.push(lowColor);
 
@@ -35,56 +35,109 @@ const colorListEl = document.getElementById('colorList');
 // Add event listener to the form tag when a submit event occurs
 formEl.addEventListener('submit', colorMatch);
 // The function for it will test the color entered in the input matches a dictionary of colors used in html. if so it will create a new list element with the colorname and a check box.
-function colorMatch(e){
+function colorMatch(e) {
     e.preventDefault();
     //alert ('submit event occured');
     const colorValCheck = validateColor();
-    if(!colorValCheck){
-        alert ('Color does not exist on this page');
+    if (!colorValCheck) {
+        alert('Color does not exist on this page');
         return;
     }
     let colorVal = colorInputEl.value.toUpperCase();
     const listEl = document.createElement('li');
     listEl.innerHTML = `<input type="checkbox" id="${colorVal}"><label for="${colorVal}">${colorVal.toUpperCase()}</label><br>`;
-    
-    
+
+
 
     // before adding it to ul we will iterate over the li labels to make sure that color doesnt already exist in the list
     const colorsList = document.querySelectorAll('label');
-    if(colorsList.length != 0){
-        for(color of colorsList){
-        console.log(color.textContent);
-        if(color.textContent == colorVal){
-            alert ('color has already been added');
-            return;
-        } else{
-            colorListEl.appendChild(listEl);
+    if (colorsList.length != 0) {
+        for (color of colorsList) {
+            console.log(color.textContent);
+            if (color.textContent == colorVal) {
+                alert('color has already been added');
+                colorInputEl.value = '';
+                return;
+            } else {
+                colorListEl.appendChild(listEl);
+            }
         }
-    }
-    } else{
+    } else {
         colorListEl.appendChild(listEl);
+
     }
-    
-    
-    
+
+
+
+    colorInputEl.value = '';
+
+
+    // Iterate through all inputs that are children of the <li>'s if checked = 'true' set the background-color of the body equal to the id attribute of that input. Use setInterval to set time to cycle through the different colors.
+
+
     
 
 
 
 }
 // Create event listern for the ul 
-    // when a checkbox is checked in a li it cyles the color from that label in the background of the body
-    
+// when a checkbox is checked in a li it cyles the color from that label in the background of the body
+// let checkedColors = colorListEl.getElementsByTagName('input');
+// const body = document.getElementById('bigBody');
+
+// console.log(colorListEl);
+
+//checkedColors.addEventListener('change', colorChanger);
+
+let checkedColors = colorListEl.getElementsByTagName('input');
+const body = document.getElementById('bigBody');
+
+console.log(checkedColors);
+
+colorListEl.addEventListener('change', colorChanger);
+
+function colorChanger(e) {
+    const colorId = e.target.getAttribute("id");
+    if(e.target.checked){
+    alert(`${colorId} has been checked`);
+    }
 
 
-    // helper functions:
-    // validateColor function to check that the color entered is within the array of possible html colors;
-    function validateColor(){
-        let colorVal = colorInputEl.value.toLowerCase();
-        if(colorArr.includes(colorVal)){
-            return true;
-        }
-        else return false;
 
-    };
-    
+
+}
+
+
+// function colorChanger(e) {
+//     console.log(e.target);
+//     // if(e.checked){
+//     //     alert('box is checked');
+//     // }
+
+
+
+
+// }
+
+// for (selected of checkedColors) {
+//         if (!selected.checked) {
+//             continue;
+//         } else {
+//             body.style.backgroundColor = selected.getAttribute('id');
+
+//         }
+//     }
+
+
+
+
+// helper functions:
+// validateColor function to check that the color entered is within the array of possible html colors;
+function validateColor() {
+    let colorVal = colorInputEl.value.toLowerCase();
+    if (colorArr.includes(colorVal)) {
+        return true;
+    }
+    else return false;
+
+};
